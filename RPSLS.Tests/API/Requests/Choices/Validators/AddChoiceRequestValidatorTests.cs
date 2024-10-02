de846e7b9ad61cde8181b40341fc8170372b91db
SoviljIvana <isovilj1@gmail.com>
2024-10-03 00:42:45 +0200

    feature/initial-setup files reorganization, added tests, clients

---
+++ RPSLS.Tests/API/Requests/Choices/Validators/AddChoiceRequestValidatorTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using FluentValidation.TestHelper;
+using RPSLS.API.Requests.Choices;
+using RPSLS.API.Requests.Choices.Validators;
+using RPSLS.Tests.API.Customization;
+using Xunit;
+
+namespace RPSLS.Tests.API.Requests.Choices.Validators
+{
+    public class AddChoiceRequestValidatorTests
+    {
+        [Theory]
+        [AutoMoqData]
+        public void ShouldFail_When_NameIsEmpty(AddChoiceRequestValidator sut, AddChoiceRequest request)
+        {
+            // Arrange
+            request.Name = "";
+
+            // Act
+            var result = sut.TestValidate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors[0].ErrorMessage.Should().Be("Name is mandatory field.");
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void ShouldFail_When_IdIsEmpty(AddChoiceRequestValidator sut)
+        {
+            // Arrange
+            AddChoiceRequest request = new()
+            {
+                Name = "paper"
+            };
+
+            // Act
+            var result = sut.TestValidate(request);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors[0].ErrorMessage.Should().Be("Id is mandatory field.");
+        }
+    }
+}
